4b4f7c395c49e333a4ff612e219c54fe21f1c29d
jimmys20 <maragkosd12@gmail.com>
2025-06-05 13:40:48 +0300

    Add project files.

---
+++ redisomdemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using Redis.OM;
+using Redis.OM.Modeling;
+
+namespace redisomdemo;
+
+[Document(StorageType = StorageType.Json, Prefixes = ["Player"])]
+public class PlayerEntity
+{
+  [RedisIdField, Indexed] public string UniquePlayerId { get; set; }
+  public DateTime EnteredAt { get; set; }
+}
+
+[Document(StorageType = StorageType.Json, Prefixes = ["PlayerState"])]
+public class PlayerStateEntity
+{
+  [RedisIdField, Indexed] public string UniquePlayerId { get; set; }
+}
+
+internal class Program
+{
+  static async Task Main(string[] args)
+  {
+    var redisConfigurationOptions = new StackExchange.Redis.ConfigurationOptions
+    {
+      EndPoints = { "192.168.100.4:6379" },
+      User = null,
+      Password = null,
+      AllowAdmin = false,
+      Ssl = false,
+    };
+
+    var provider = new RedisConnectionProvider(redisConfigurationOptions);
+
+    provider.Connection.DropIndexAndAssociatedRecords(typeof(PlayerEntity));
+    await provider.Connection.CreateIndexAsync(typeof(PlayerEntity));
+
+    provider.Connection.DropIndexAndAssociatedRecords(typeof(PlayerStateEntity));
+    await provider.Connection.CreateIndexAsync(typeof(PlayerStateEntity));
+
+    var players = provider.RedisCollection<PlayerEntity>();
+    var playerStates = provider.RedisCollection<PlayerStateEntity>();
+
+    var uniquePlayerId = Guid.NewGuid().ToString();
+
+    await players.InsertAsync(new PlayerEntity
+    {
+      UniquePlayerId = uniquePlayerId,
+      EnteredAt = DateTime.UtcNow,
+    });
+
+    var playerList = await players.ToListAsync();
+
+    await playerStates.InsertAsync(new PlayerStateEntity
+    {
+      UniquePlayerId = uniquePlayerId,
+    });
+
+    var playerList2 = await players.ToListAsync();
+  }
+}
---
+++ redisomdemo2/Program.cs
@@ -0,0 +1,65 @@
+﻿using Redis.OM;
+using Redis.OM.Modeling;
+
+namespace redisomdemo2;
+
+
+
+[Document(StorageType = StorageType.Json, Prefixes = ["PlayerState"])]
+public class PlayerStateEntity
+{
+  [RedisIdField, Indexed] public string UniquePlayerId { get; set; }
+  //public DateTimeOffset StartTime { get; set; }
+}
+
+internal class Program
+{
+  static async Task Main(string[] args)
+  {
+    var redisConfigurationOptions = new StackExchange.Redis.ConfigurationOptions
+    {
+      EndPoints = { "192.168.100.4:6379" },
+      User = null,
+      Password = null,
+      AllowAdmin = false,
+      Ssl = false,
+    };
+
+    var provider = new RedisConnectionProvider(redisConfigurationOptions);
+
+    provider.Connection.DropIndexAndAssociatedRecords(typeof(PlayerStateEntity));
+    await provider.Connection.CreateIndexAsync(typeof(PlayerStateEntity));
+
+
+    var playerStates = provider.RedisCollection<PlayerStateEntity>();
+
+    var uniquePlayerId = "abc-234";
+    //var startTime = new DateTimeOffset(2024, 11, 21, 12, 30, 00, new TimeSpan(1, 0, 0));
+
+    await playerStates.InsertAsync(new PlayerStateEntity
+    {
+      UniquePlayerId = uniquePlayerId,
+      //StartTime = startTime,
+    });
+
+    return;
+
+    var player = await playerStates.FindByIdAsync(uniquePlayerId);
+
+    //await players.SaveAsync();
+
+    await playerStates.InsertAsync(new PlayerStateEntity
+    {
+      UniquePlayerId = uniquePlayerId,
+      //StartTime = startTime,
+    });
+
+    var player2 = await playerStates.FindByIdAsync(uniquePlayerId);
+
+    var x = await playerStates.ToListAsync();
+
+    //Console.WriteLine(player.StartTime);
+    //Console.WriteLine(player2.StartTime);
+    //Console.WriteLine(player.StartTime.Offset == player2.StartTime.Offset);
+  }
+}
